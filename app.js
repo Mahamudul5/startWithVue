2d876bc0905050cc93db1402388fec528691c5c2
hasibbd <hasib.0951@gmail.com>
2022-06-20 12:14:32 +0600

    v-text learned

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 new Vue({
 el:"#app",
 data:{
-    title:"allah is a best planner"
+    title:"allah is the best planner",
+    src: "https://picsum.photos/200/300",
+    link:"http://google.com",
+    myText:"hello learner",
+    myBoldText:"<strong>my future</strong>"
 }
 });
