14f5915a35ea20bcf9c25684d45e59e856660524
hasibbd <hasib.0951@gmail.com>
2022-06-20 12:38:54 +0600

    v-if dine

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 new Vue({
 el:"#app",
 data:{
-    title:"allah is the best planner",
-    src: "https://picsum.photos/200/300",
-    link:"http://google.com",
-    myText:"hello learner",
-    myBoldText:"<strong>my future</strong>"
+ userName:"Mahamudul",
+ age:16,
+ allowedAge:20
+
+},
+methods:{
+changeUserName(){
+    return this.userName==="hhhs" ? true:false
+}
+
 }
 });
