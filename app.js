40b0a503e280e2108b35d605cdc5a477f9f29a4e
hasibbd <hasib.0951@gmail.com>
2022-06-20 14:57:46 +0600

    cordinate find

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ new Vue({
 el:"#app",
 data:
 {
-name:'bilash'
+name:'bilash',
+x:0,
+y:0
 },
 methods: {
     updateName(){
         this.name='mahamudul';
+       },
+       getCord(event){
+this.x=event.clientX;
+this.y=event.clientY;
        }
 },
 });
