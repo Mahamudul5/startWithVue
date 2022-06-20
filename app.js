8181648de0aaa78605a7755599e670b28f40d61e
hasibbd <hasib.0951@gmail.com>
2022-06-20 14:42:16 +0600

    event

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ data:
 name:'bilash'
 },
 methods: {
-    update(){
-        setTimeout(()=>{
-            this.name='mahamudul'
-        },2000)
-    }
+    updateName(){
+        this.name='mahamudul';
+       }
 },
 });
